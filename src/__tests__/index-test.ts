969d8f0e78823699411e30b47dd5a0b9e960a148
Daniel Schmidt <dschmidt@mesosphere.com>
2018-09-01 23:16:34 +0200

    feat: add conditional tracing for requests

--- src/__tests__/index-test.ts
+++ src/__tests__/index-test.ts
@@ -71,6 +71,7 @@ describe("Apollo Tracing", () => {
       cb();
       expect(server.span.finish).toHaveBeenCalled();
     });
+
     it("starts and finishes a request spans if there are errors", () => {
       const cb = tracingMiddleware.requestDidStart({ queryString: "query {}" });
       expect(server.startSpan).toHaveBeenCalled();
@@ -80,23 +81,50 @@ describe("Apollo Tracing", () => {
       cb(new Error("ups"));
       expect(server.span.finish).toHaveBeenCalled();
     });
+
+    it("predicate gets called with same arguments as the middleware", () => {
+      const shouldTraceRequest = jest.fn();
+      tracingMiddleware = new ApolloOpentracing({
+        server,
+        local,
+        shouldTraceRequest
+      });
+
+      tracingMiddleware.requestDidStart({ queryString: "query {}" });
+      expect(shouldTraceRequest).toHaveBeenCalledWith({
+        queryString: "query {}"
+      });
+    });
+
+    it("doesn't start spans when corresponding predicate returns false", () => {
+      const shouldTraceRequest = jest.fn().mockReturnValue(false);
+      tracingMiddleware = new ApolloOpentracing({
+        server,
+        local,
+        shouldTraceRequest
+      });
+
+      tracingMiddleware.requestDidStart({ queryString: "query {}" });
+      expect(server.startSpan).not.toHaveBeenCalled();
+    });
   });
 
   describe("field resolver", () => {
     it("starts a new local span for the field", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       tracingMiddleware.willResolveField({}, {}, {}, {});
       expect(server.startSpan).not.toHaveBeenCalled();
       expect(local.startSpan).toHaveBeenCalled();
     });
 
     it("uses the 'fieldname' as the span name if no fieldname can be found", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       tracingMiddleware.willResolveField({}, {}, {}, {});
-      expect(local.startSpan).toHaveBeenCalledWith("field", {
-        childOf: undefined
-      });
+      expect(local.startSpan).toHaveBeenCalledWith("field", expect.any(Object));
     });
 
     it("uses the name as the span name if no fieldname can be found", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       tracingMiddleware.willResolveField(
         {},
         {},
@@ -105,25 +133,42 @@ describe("Apollo Tracing", () => {
           fieldName: "myField"
         }
       );
-      expect(local.startSpan).toHaveBeenCalledWith("myField", {
-        childOf: undefined
-      });
+      expect(local.startSpan).toHaveBeenCalledWith(
+        "myField",
+        expect.any(Object)
+      );
     });
 
-    it("starts the span as a child span", () => {
+    it("starts the span as a child span of another field", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       tracingMiddleware.willResolveField({}, {}, { span: { id: "42" } }, {});
       expect(local.startSpan).toHaveBeenCalledWith("field", {
         childOf: { id: "42" }
       });
     });
 
+    it("starts the span as a child span of the request", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
+      tracingMiddleware.willResolveField({}, {}, {}, {});
+      expect(local.startSpan).toHaveBeenCalledWith("field", {
+        childOf: { id: "23" }
+      });
+    });
+
+    it("does not start a span if there is no request span", () => {
+      tracingMiddleware.willResolveField({}, {}, {}, {});
+      expect(local.startSpan).not.toHaveBeenCalled();
+    });
+
     it("adds the span to the context", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       const ctx = {};
       tracingMiddleware.willResolveField({}, {}, ctx, {});
       expect(ctx.span).toBeDefined();
     });
 
     it("logs an error", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       const err = new Error("my-error");
 
       const cb = tracingMiddleware.willResolveField({}, {}, {}, {});
@@ -135,6 +180,7 @@ describe("Apollo Tracing", () => {
     });
 
     it("logs a result", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       const result = { data: { id: "42" } };
 
       const cb = tracingMiddleware.willResolveField({}, {}, {}, {});
@@ -144,7 +190,9 @@ describe("Apollo Tracing", () => {
         result: JSON.stringify(result)
       });
     });
+
     it("finishes the span", () => {
+      tracingMiddleware.requestSpan = { id: "23" };
       const cb = tracingMiddleware.willResolveField({}, {}, {}, {});
       cb();
       expect(local.span.finish).toHaveBeenCalled();
