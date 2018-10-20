d0bacea484b5ae78806b56dcb446eb4cd4ff2531
Ciaran Liedeman <ciaranliedeman@gmail.com>
2018-10-20 12:54:31 +0200

    test: Span serializer

---
+++ src/test/span-serializer.ts
@@ -0,0 +1,58 @@
+import "jest";
+import { MockSpanTree } from "./types";
+
+const TAB = "   ";
+
+function prefix(depth: number) {
+  return TAB.repeat(depth) + `+-- `;
+}
+
+function logLine(log: any, index: number, depth: number) {
+  return `${TAB.repeat(depth + 1)}${index}. ${JSON.stringify(log).replace(
+    /\\"/g,
+    ""
+  )}`;
+}
+
+function logs(span: MockSpanTree, depth: number) {
+  if (span.logs && span.logs.length > 0) {
+    return `${TAB.repeat(depth + 1)}logs:\n${span.logs
+      .map((log, index) => logLine(log, index + 1, depth))
+      .join("\n")}\n`;
+  }
+
+  return "";
+}
+
+function tag(span: MockSpanTree, depth: number) {
+  return `${span.name}:${span.id}\n${TAB.repeat(depth + 1)}finished: ${
+    span.finished
+  }\n${logs(span, depth)}`;
+}
+
+function buildSpan(span: MockSpanTree, depth = 0) {
+  let result = "";
+
+  result += tag(span, depth);
+
+  if (span.children) {
+    for (const child of span.children) {
+      result += `${prefix(depth)}${buildSpan(child, depth + 1)}`;
+    }
+  }
+
+  return result;
+}
+
+export default {
+  test: (val: any) => val.id && val.name && val.logs && val.finished != null,
+  print(
+    val: any,
+    _serialize: ((val: any) => string),
+    _indent: ((str: string) => string),
+    _opts: jest.SnapshotSerializerOptions,
+    _colors: jest.SnapshotSerializerColors
+  ) {
+    return buildSpan(val as MockSpanTree);
+  }
+};
